f41169120de51258e68bdfe3b7586d8834b1e2dc
Jo-wonbin <jwb7214@gmail.com>
2022-11-29 20:00:15 +0900

    Feat : 221129 문제풀이

---
+++ src/Programmers/Programmers_p와y갯수.java
@@ -0,0 +1,24 @@
+package Programmers;
+
+public class Programmers_p와y갯수 {
+    class Solution {
+        boolean solution(String s) {
+
+            int p = 0;
+            int y = 0;
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == 'p' || s.charAt(i) == 'P') {
+                    p++;
+                } else if (s.charAt(i) == 'y' || s.charAt(i) == 'Y') {
+                    y++;
+                }
+            }
+
+            if (p == y) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+}
