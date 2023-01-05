172e9f19d9a646a7963cdb3836ee4a377bcb2299
Jo-wonbin <78211128+Jo-wonbin@users.noreply.github.com>
2023-01-06 01:42:50 +0900

    [Silver IV] Title: 수들의 합 2, Time: 176 ms, Memory: 15076 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -1,48 +1,56 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int N, M, count;
+    static boolean[] visited;
+    static int[] arr;
 
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int n = Integer.parseInt(st.nextToken());
-		long m = Long.parseLong(st.nextToken());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		long list[] = new long[n];
-		st = new StringTokenizer(br.readLine(), " ");
-		for (int i = 0; i < n; i++) {
-			list[i] = Long.parseLong(st.nextToken());
-		}
-		long result;
-		int cnt = 0;
-		int count = 0;
-		int tcnt = 0;
-		while (tcnt <= n - 1) {
-			result = 0;
-			for (int i = count; i < n; i++) {
-				result += list[i];
-				if (result == m) {
-					count++;
-					cnt++;
-					break;
-				} else if (result > m) {
-					count++;
-					break;
-				}
-			}
-			tcnt++;
-		}
-		bw.write(cnt + "");
-		br.close();
-		bw.flush();
-		br.close();
-	}
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+        visited = new boolean[N];
+
+        st = new StringTokenizer(br.readLine());
+
+        int cnt = 0;
+        while (cnt < N) {
+            arr[cnt++] = Integer.parseInt(st.nextToken());
+        }
+        count = 0;
+
+        int start = 0;
+        int end = 0;
+        int temp = 0;
+        while(start <= end && start < N && end < N){
+            temp += arr[end];
+            end++;
+            if(temp == M){
+                count++;
+                start++;
+                end = start;
+                temp = 0;
+            }else if(temp > M){
+                start++;
+                end = start;
+                temp = 0;
+            }
+        }
+
+        System.out.println(count);
+
+        br.close();
+    }
+
+
 
 }
