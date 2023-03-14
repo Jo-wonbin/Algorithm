a31d56ce2a87a0b868a16eb5e4636437ba44c54a
Jo-wonbin <78211128+Jo-wonbin@users.noreply.github.com>
2023-03-14 19:51:36 +0900

    [level 2] Title: 전화번호 목록, Time: 287.11 ms, Memory: 92 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phoneBook) {
+        boolean answer = true;
+        
+        Arrays.sort(phoneBook);
+        for(int i=0; i<phoneBook.length-1; i++){
+            
+            if(phoneBook[i+1].startsWith(phoneBook[i]))
+                return false;
+            
+        }
+        
+        return answer;
+    }
+}
