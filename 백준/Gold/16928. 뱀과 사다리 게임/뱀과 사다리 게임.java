1afdccc29f29dba90cbe008baa17a9f84ff1ba22
WonbinJo <78211128+Jo-wonbin@users.noreply.github.com>
2023-07-05 15:12:39 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 128 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
@@ -66,29 +66,13 @@ public class Main {
                 temp = snakes.get(now.x);
             }
 
-            if (temp + 1 <= 100 && !visited[temp + 1]) {
-                visited[temp + 1] = true;
-                q.add(new Point(temp + 1, now.depth + 1));
-            }
-            if (temp + 2 <= 100 && !visited[temp + 2]) {
-                visited[temp + 2] = true;
-                q.add(new Point(temp + 2, now.depth + 1));
-            }
-            if (temp + 3 <= 100 && !visited[temp + 3]) {
-                visited[temp + 3] = true;
-                q.add(new Point(temp + 3, now.depth + 1));
-            }
-            if (temp + 4 <= 100 && !visited[temp + 4]) {
-                visited[temp + 4] = true;
-                q.add(new Point(temp + 4, now.depth + 1));
-            }
-            if (temp + 5 <= 100 && !visited[temp + 5]) {
-                visited[temp + 5] = true;
-                q.add(new Point(temp + 5, now.depth + 1));
-            }
-            if (temp + 6 <= 100 && !visited[temp + 6]) {
-                visited[temp + 6] = true;
-                q.add(new Point(temp + 6, now.depth + 1));
+            for (int i = 1; i <= 6; i++) {
+                if (temp + i > 100)
+                    continue;
+                if (visited[temp + i])
+                    continue;
+                visited[temp + i] = true;
+                q.add(new Point(temp + i, now.depth + 1));
             }
         }
     }
