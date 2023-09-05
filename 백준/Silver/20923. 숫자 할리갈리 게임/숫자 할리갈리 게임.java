c8e9a016000692944ebf7d52c191e6849cfbbbd4
WonbinJo <78211128+Jo-wonbin@users.noreply.github.com>
2023-09-05 16:52:52 +0900

    [Silver I] Title: 숫자 할리갈리 게임, Time: 648 ms, Memory: 91640 KB -BaekjoonHub

---
+++ 백준/Silver/20923. 숫자 할리갈리 게임/숫자 할리갈리 게임.java
@@ -0,0 +1,94 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        Queue<Integer> dodo = new LinkedList<>();
+        Queue<Integer> suyeon = new LinkedList<>();
+
+        ArrayList<Integer> groundOfDodo = new ArrayList<>();
+        ArrayList<Integer> groundOfSuyeon = new ArrayList<>();
+
+        Stack<Integer> st1 = new Stack<>();
+        Stack<Integer> st2 = new Stack<>();
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            st1.push(Integer.parseInt(st.nextToken()));
+            st2.push(Integer.parseInt(st.nextToken()));
+        }
+
+        for (int i = 0; i < N; i++) {
+            dodo.add(st1.pop());
+            suyeon.add(st2.pop());
+        }
+
+        int topOfDodo = 0;
+        int topOfSuyeon = 0;
+        for (int i = 0; i < M; i++) {
+            if (i % 2 == 0) {
+                topOfDodo = dodo.poll();
+                groundOfDodo.add(topOfDodo);
+            } else {
+                topOfSuyeon = suyeon.poll();
+                groundOfSuyeon.add(topOfSuyeon);
+            }
+
+            if (dodo.size() == 0) {
+                System.out.println("su");
+                return;
+            }
+            if (suyeon.size() == 0) {
+                System.out.println("do");
+                return;
+            }
+
+            if (topOfDodo != 0 && topOfSuyeon != 0) {
+                if (topOfDodo + topOfSuyeon == 5) {
+
+                    groundOfDodo.forEach(suyeon::add);
+                    groundOfDodo.clear();
+                    groundOfSuyeon.forEach(suyeon::add);
+                    groundOfSuyeon.clear();
+
+                    topOfDodo = 0;
+                    topOfSuyeon = 0;
+
+                }
+            }
+            if (topOfDodo == 5 || topOfSuyeon == 5) {
+
+                groundOfSuyeon.forEach(dodo::add);
+                groundOfSuyeon.clear();
+                groundOfDodo.forEach(dodo::add);
+                groundOfDodo.clear();
+
+                topOfDodo = 0;
+                topOfSuyeon = 0;
+            }
+
+        }
+
+        if (dodo.size() > suyeon.size()) {
+            System.out.println("do");
+        } else if (dodo.size() < suyeon.size()) {
+            System.out.println("su");
+        } else {
+            System.out.println("dosu");
+        }
+
+        br.close();
+    }
+
+}
