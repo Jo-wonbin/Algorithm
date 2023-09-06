4307ea730c4348b45567f2e8c8ae226cf79cba96
WonbinJo <78211128+Jo-wonbin@users.noreply.github.com>
2023-09-06 17:11:11 +0900

    [Silver I] Title: 후보 추천하기, Time: 144 ms, Memory: 14408 KB -BaekjoonHub

--- 백준/Silver/1713. 후보 추천하기/후보 추천하기.java
+++ 백준/Silver/1713. 후보 추천하기/후보 추천하기.java
@@ -6,6 +6,7 @@ import java.util.*;
 public class Main {
 
     static class Student implements Comparable<Student> {
+        // 각각 후보의 추천수, 숫자, 등록 시간
         int good, number, cnt;
 
         Student(int good, int number, int cnt) {
@@ -14,6 +15,7 @@ public class Main {
             this.cnt = cnt;
         }
 
+        // 추천 수 기준 오름차순 정렬, 추천 수가 같으면 등록 시간이 오래된 것이 우선
         @Override
         public int compareTo(Student o) {
             if (o.good == this.good) {
@@ -33,28 +35,43 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         for (int i = 0; i < times; i++) {
-            int temp = Integer.parseInt(st.nextToken());
 
+            // 후보의 숫자
+            int numberOfStudent = Integer.parseInt(st.nextToken());
+
+            // 사진틀에 있는 후보 판별 변수
             boolean flag = false;
+
+            // 해당 후보의 인덱스
             int index = 0;
+
+            // 사진틀에 후보가 있는지 판단하여 해당 인덱스를 반환
             for (int j = 0; j < students.size(); j++) {
-                if (students.get(j).number == temp) {
+                if (students.get(j).number == numberOfStudent) {
                     index = j;
                     flag = true;
                     break;
                 }
             }
+
+            // 사진틀에 후보가 있으면 해당 후보의 추천수 증가
             if (flag) {
                 students.get(index).good++;
-            } else {
+            }
+            // 후보가 없다면
+            else {
+                // 사진틀이 꽉찼으면 정렬하여 추천수가 가장 적고 등록된지 가장 오래된 후보 삭제
                 if (students.size() == N) {
                     Collections.sort(students);
                     students.remove(0);
                 }
-                students.add(new Student(1, temp, i));
+                // 사진틀에 후보 넣기
+                students.add(new Student(1, numberOfStudent, i));
             }
 
         }
+
+        // 사진틀에 등록된 후보의 숫자를 오름차순으로 정렬하여 출력할 리스트
         ArrayList<Integer> result = new ArrayList<>();
 
         StringBuilder sb = new StringBuilder();
@@ -66,6 +83,7 @@ public class Main {
             sb.append(tp).append(" ");
         }
         System.out.println(sb);
+
         br.close();
     }
 
