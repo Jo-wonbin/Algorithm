9c1b329b9203f72439cf4ca5de16fb28971e80e9
WonbinJo <78211128+Jo-wonbin@users.noreply.github.com>
2024-01-04 12:00:27 +0900

    [Silver V] Title: 카드1, Time: 128 ms, Memory: 14344 KB -BaekjoonHub

---
+++ 백준/Silver/2161. 카드1/카드1.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for(int i=1; i<=N; i++){
+            q.add(i);
+        }
+
+        Queue<Integer> qq = new LinkedList<>();
+        while(q.size() > 1 ){
+            qq.add(q.poll());
+            q.add(q.poll());
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while(!qq.isEmpty()){
+            sb.append(qq.poll()).append(" ");
+        }
+        sb.append(q.poll());
+
+        System.out.println(sb);
+
+        br.close();
+    }
+}
