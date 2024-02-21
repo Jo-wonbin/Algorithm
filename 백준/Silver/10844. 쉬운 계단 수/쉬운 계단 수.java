9b5043bd6d33e6ed4fd0cee063c737e56737e4e7
WonbinJo <78211128+Jo-wonbin@users.noreply.github.com>
2024-02-21 13:08:14 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 124 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        final long MOD = 1000000000L;
+
+        int N = Integer.parseInt(br.readLine());
+
+        long dp[][] = new long[N + 1][10];
+
+        for (int i = 1; i < 10; i++) {
+            dp[1][i] = 1;
+        }
+        if (N >= 2) {
+            for (int i = 2; i <= N; i++) {
+                for (int j = 0; j <= 9; j++) {
+                    if (j == 0) {
+                        dp[i][j] = dp[i - 1][1] % MOD;
+                    } else if (j == 9) {
+                        dp[i][j] = dp[i - 1][8] % MOD;
+                    } else {
+                        dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % MOD;
+                    }
+                }
+            }
+        }
+
+        long result = 0;
+        for (int i = 0; i <= 9; i++) {
+            result = (result + dp[N][i]) % MOD;
+        }
+        System.out.println(result);
+        br.close();
+    }
+}
