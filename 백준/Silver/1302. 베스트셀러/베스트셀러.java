fdaab0468520744434cecebe29ecf5cc95839f8f
WonbinJo <78211128+Jo-wonbin@users.noreply.github.com>
2024-02-29 10:15:28 +0900

    [Silver IV] Title: 베스트셀러, Time: 124 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            String temp = st.nextToken();
+            map.put(temp, map.getOrDefault(temp, 0) + 1);
+        }
+
+        List<String> keySet = new ArrayList<>(map.keySet());
+
+        keySet.sort(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if (map.get(o1) == (map.get(o2))) {
+                    return o1.compareTo(o2);
+                }
+                return map.get(o2).compareTo(map.get(o1));
+            }
+        });
+
+        System.out.println(keySet.get(0));
+        br.close();
+    }
+}
