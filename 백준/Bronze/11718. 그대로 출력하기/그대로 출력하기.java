4170514fedc54b3a88c8490c352379b03f498e16
WonbinJo <78211128+Jo-wonbin@users.noreply.github.com>
2024-03-12 13:33:48 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 124 ms, Memory: 14344 KB -BaekjoonHub

--- 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -7,11 +7,13 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        StringBuilder sb = new StringBuilder();
         String line;
         while ((line = br.readLine()) != null){
-            System.out.println(line);
+            sb.append(line).append("\n");
         }
 
+        System.out.println(sb);
         br.close();
     }
 
